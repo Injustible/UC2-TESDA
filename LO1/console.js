bf3c67fcd88bfe76531390929dfa7b64b3fd1aa9
Injustible <78533502+Injustible@users.noreply.github.com>
2021-02-06 17:50:36 +0800

    Update console.js

--- LO1/console.js
+++ LO1/console.js
@@ -2,25 +2,25 @@
 
 console.clear(); //clear console
 
-console.log ('Mic test mic test'); // string
+console.log('Mic test mic test'); // string
 
-console.log (2021); // integer
+console.log(2021); // integer
 
-console.log (true); // boolean
+console.log(true); // boolean
 
 var greetings = 'Kamusta?' //variable
-console.log (greetings);
+console.log(greetings);
 
 let greetingsV2 = 'Kamusta ka na?' //let
-console.log (greetingsV2);
+console.log(greetingsV2);
 
-console.log ([1,2,3,4,5,6,7,8]); //array
+console.log([1,2,3,4,5,6,7,8]); //array
 
-console.log ({ten: 10, five: 5}); //object
+console.log({ten: 10, five: 5}); //object
 
-console.table ({ten: 10, five: 5}); //table display object
+console.table({ten: 10, five: 5}); //table display object
 
-console.error ('Bluescreen Error'); //error message
+console.error('Bluescreen Error'); //error message
 
 console.warn('Warning Beware!!!'); //warning message
 
